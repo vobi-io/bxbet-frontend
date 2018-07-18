9d2d9ea48832c04b47c04c5b5c79402277b0636c
Giga <giga.chkhikvadze@gmail.com>
2018-07-18 16:59:51 +0400

    frone end

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
   apiUrl: 'https://bx.vobi.io',
-  apiSocketUrl: 'http://bx.vobi.io',
+  apiSocketUrl: 'https://wsbx.vobi.io/',
   apiGraphqlUrl: 'https://bx.vobi.io/graphql',
 }
--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   apiUrl: 'https://bx.vobi.io',
-  apiSocketUrl: 'http://bx.vobi.io',
+  apiSocketUrl: 'hhttps://wsbx.vobi.io/',
   apiGraphqlUrl: 'https://bx.vobi.io/graphql',
 }
